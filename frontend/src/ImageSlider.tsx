b5858cd9ec5ba86e5e875b7d32b9dd5df0c4b013
OrGaNiK <OrGaNiK777@yandex.ru>
2024-09-03 16:53:20 +0500

    03.09.24v2

--- frontend/src/ImageSlider.tsx
+++ frontend/src/ImageSlider.tsx
@@ -20,8 +20,8 @@ const ImageSlider: React.FC = () => {
 	}
 
 	return (
-	<div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}} className='relative mw-[1240px] h-[max] overflow-hidden'>
-			<div className='flex transition-transform duration-300 ease-in-out' style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+		<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} className='relative mw-[1240px] h-[max] overflow-hidden'>
+			<div className='flex transition-transform duration-300 ease-in-out' style={{ transform: `translateX(-${currentIndex * 100}%)`, maxWidth: '1240px'}}>
 				{images.map((image, index) => (
 					<img style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} key={index} src={'http://localhost:3000/images' + image} className='w-[1240px] h-[836px] object-cover flex-shrink-0' alt={image} />
 				))}
